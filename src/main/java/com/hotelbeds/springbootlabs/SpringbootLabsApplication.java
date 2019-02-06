746782b331cd4a92dee427af14e471a14851fbb3
Alberto Frontera Morales <afrontera@hotelbeds.com>
2019-02-06 11:36:23 +0100

    Initial commit

---
+++ src/main/java/com/hotelbeds/springbootlabs/SpringbootLabsApplication.java
@@ -0,0 +1,14 @@
+package com.hotelbeds.springbootlabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLabsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootLabsApplication.class, args);
+    }
+
+}
+
